fd2002aab13903f8b21f066820b6e3e11fda02de
Christian Neumann <cneumann@datenkarussell.de>
2012-03-10 11:53:46 +0100

    Released version 0.1.0b2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools import Command
 import sys, os
 
-version = '0.1.0b'
+version = '0.1.0b2'
 
 class PyTest(Command):
     user_options = []
