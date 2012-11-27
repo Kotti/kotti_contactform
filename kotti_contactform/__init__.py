c9ebcdc6809771a0411ac6a4c2af5eb1af923edf
Marco Scheidhuber <marco@jusid.de>
2012-11-27 15:03:57 +0100

    Use pyramid.includes instead of deprecated kotti.includes.

--- kotti_contactform/__init__.py
+++ kotti_contactform/__init__.py
@@ -1,5 +1,5 @@
 def kotti_configure(settings):
-    settings['kotti.includes'] += ' kotti_contactform.views'
+    settings['pyramid.includes'] += ' kotti_contactform.views'
     settings['kotti.available_types'] += (
         ' kotti_contactform.resources.ContactForm'
     )
