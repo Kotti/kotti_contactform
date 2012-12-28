a2c4fbe8f5625991ffe05091c1b8abc300fce5aa
Marco Scheidhuber <j23d@jusid.de>
2012-12-28 13:27:58 +0100

    Back to dev.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_require = [
 ]
 
 setup(name='kotti_contactform',
-      version='0.1.1',
+      version='0.2dev',
       description="Simple contact form for Kotti sites",
       long_description=README + '\n\n' + CHANGES,
       keywords='kotti contact form',
