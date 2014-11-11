b4873df44175c21d262374995c23816a090ce7e1
Andreas Kaiser <disko@binary-punks.com>
2014-11-11 13:23:41 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_requires = [
 
 
 setup(name='kotti_contactform',
-      version='0.4dev',
+      version='0.4',
       description="Simple contact form for Kotti sites",
       long_description=README + '\n\n' + CHANGES,
       keywords='kotti contact form',
